957818ead24217d94638a1526fcc9fb7fd5a3145
Adrian Möller <adrian.moeller@stud.tu-darmstadt.de>
2024-03-01 17:19:56 +0100

    Expose receive function

--- kernel/handshake/netinet/hyquic.h
+++ kernel/handshake/netinet/hyquic.h
@@ -15,3 +15,4 @@ struct hyquic_frame {
 
 int hyquic_set_transport_parameter(int sockfd, const void *param, size_t param_length, const struct hyquic_frame_details *frame_details, size_t num_frame_details);
 int hyquic_send_frames(int sockfd, const struct hyquic_frame *frames, size_t num_frames, size_t total_frame_data_length);
+int hyquic_receive(int sockfd, const struct hyquic_receive_ops *recv_ops, size_t len);
