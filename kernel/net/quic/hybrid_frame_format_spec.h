be0c016be8191885d3d11fc629a11915b887159e
Adrian Möller <adrian.moeller@stud.tu-darmstadt.de>
2024-03-17 22:35:33 +0100

    WIP: frame format specification

--- kernel/net/quic/hybrid_frame_format_spec.h
+++ kernel/net/quic/hybrid_frame_format_spec.h
@@ -1,27 +1,96 @@
 #ifndef __HYBRID_FRAME_FORMAT_SPEC_H__
 #define __HYBRID_FRAME_FORMAT_SPEC_H__
 
+#define HYQUIC_REF_ID_MAX 63
+
 struct hyquic_frame_format_spec_cont {
     uint16_t spec_length;
-    const uint8_t *spec_cursor;
-    const uint8_t *content_cursor;
+    uint8_t *spec_cursor;
+    uint32_t content_length;
+    uint8_t *content_cursor;
     uint32_t parsed_len;
-    uint64_t ref_id_index[63];
+    uint64_t ref_id_index[HYQUIC_REF_ID_MAX];
 };
 
-static inline hyquic_parse_var_int_component(struct hyquic_frame_format_spec_cont *cont, uint8_t ref_id)
+static inline int hyquic_ref_id_to_index(struct hyquic_frame_format_spec_cont *cont, uint8_t ref_id, uint64_t length_value)
 {
-    // TODO
+    if (ref_id > HYQUIC_REF_ID_MAX || cont->ref_id_index[ref_id - 1])
+        return -EINVAL;
+    cont->ref_id_index[ref_id - 1] = length_value;
+    return 0;
 }
 
-static inline hyquic_parse_fix_len_component(struct hyquic_frame_format_spec_cont *cont, uint8_t ref_id)
+static inline int hyquic_parse_var_int_component(struct hyquic_frame_format_spec_cont *cont, uint8_t ref_id)
 {
-    // TODO
+    uint64_t value;
+    uint8_t value_len;
+
+    value_len = quic_get_var(&cont->content_cursor, &cont->content_length, &value);
+    if (!value_len)
+        return -EINVAL;
+
+    cont->parsed_len += value_len;
+    cont->spec_cursor++;
+    cont->spec_length--;
+
+    if (ref_id)
+        return hyquic_ref_id_to_index(cont, ref_id, value);
+
+    return 0;
 }
 
-static inline hyquic_parse_declared_len_component(struct hyquic_frame_format_spec_cont *cont, uint8_t ref_id)
+static inline int hyquic_parse_fix_len_component(struct hyquic_frame_format_spec_cont *cont, uint8_t ref_id)
 {
+    uint64_t length_value, declared_length;
+    uint8_t length_value_len;
+    uint32_t spec_length_tmp;
+
+    cont->spec_cursor++;
+    cont->spec_length--;
+
+    spec_length_tmp = cont->spec_length;
+    length_value_len = quic_get_var(&cont->spec_cursor, &spec_length_tmp, &length_value);
+    if (!length_value_len)
+        return -EINVAL;
+    cont->spec_length = spec_length_tmp;
+
+    cont->parsed_len += length_value;
+    cont->spec_cursor += length_value_len;
+    cont->spec_length += length_value_len;
+
+    if (ref_id) {
+        if (length_value > 4)
+            return -EINVAL;
+
+        declared_length = quic_get_int(&cont->content_cursor, length_value);
+        cont->content_length -= length_value;
+
+        return hyquic_ref_id_to_index(cont, ref_id, declared_length);
+    } else {
+        cont->content_cursor += length_value;
+        cont->content_length -= length_value;
+    }
+
+    return 0;
+}
+
+static inline int hyquic_parse_declared_len_component(struct hyquic_frame_format_spec_cont *cont, uint8_t ref_id)
+{
+    uint64_t declared_length;
+
+    if (!ref_id)
+        return -EINVAL;
+
+    declared_length = cont->ref_id_index[ref_id];
+    if (!declared_length)
+        return -EINVAL;
+
+    cont->spec_cursor++;
+    cont->spec_length--;
+
     // TODO
+
+    return 0;
 }
 
 static inline int hyquic_parse_next_spec_component(struct hyquic_frame_format_spec_cont *cont)
@@ -42,12 +111,13 @@ static inline int hyquic_parse_next_spec_component(struct hyquic_frame_format_sp
     }
 }
 
-static inline int hyquic_parse_frame_content(const uint8_t *frame_content, const uint8_t *format_specification, uint16_t spec_length, uint32_t *parsed_length)
+static inline int hyquic_parse_frame_content(uint8_t *frame_content, uint32_t remaining_length, uint8_t *format_specification, uint16_t spec_length, uint32_t *parsed_length)
 {
     int err;
     struct hyquic_frame_format_spec_cont cont = {
         .spec_length = spec_length,
         .spec_cursor = format_specification,
+        .content_length = remaining_length,
         .content_cursor = frame_content,
         .parsed_len = 0,
         .ref_id_index = {0}
